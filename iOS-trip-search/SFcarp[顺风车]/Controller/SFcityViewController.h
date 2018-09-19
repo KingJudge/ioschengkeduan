f08a6a31f1ab3b9e429dec59701a7e3651ec75bf
yuanwenyi <ywy4869@live.cn>
2018-09-19 16:22:11 +0800

    瞎改改

--- iOS-trip-search/SFcarp[顺风车]/Controller/SFcityViewController.h
+++ iOS-trip-search/SFcarp[顺风车]/Controller/SFcityViewController.h
@@ -2,7 +2,7 @@
 //  SFcityViewController.h
 //  iOS-trip-search
 //
-//  Created by yaoqianghong on 2018/9/19.
+//  Created by Aydin on 2018/9/19.
 //  Copyright © 2018年 Amap. All rights reserved.
 //
 
--- iOS-trip-search/SFcarp[顺风车]/Controller/SFinviteDriverViewController.m
+++ iOS-trip-search/SFcarp[顺风车]/Controller/SFinviteDriverViewController.m
@@ -2,7 +2,7 @@
 //  SFinviteDriverViewController.m
 //  iOS-trip-search
 //
-//  Created by yaoqianghong on 2018/9/19.
+//  Created by Aydin on 2018/9/19.
 //  Copyright © 2018年 Amap. All rights reserved.
 //
 
@@ -17,7 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor yellowColor];
+    self.view.backgroundColor = [UIColor whiteColor];
     
     _backGroundImageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"ico_cross_bg"]];
     _backGroundImageView.frame = CGRectMake(0, -64, self.view.bounds.size.width, self.view.bounds.size.height);
@@ -45,17 +45,6 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
